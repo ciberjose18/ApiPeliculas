c3a9f7b62e56e196fdb66b182a2fc1c6489647bb
ciberjose18 <josemicblack@gmail.com>
2024-03-11 13:25:23 -0500

    Actualizacion

--- ApiPeliculas/obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
+++ ApiPeliculas/obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f1918c8a2a95c7ab1d96ead6c831039feb7756")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
